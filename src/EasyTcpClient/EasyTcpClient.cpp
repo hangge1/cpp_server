e4b8378ccf5415391373b430d554aae6ada0e4b6
hangdada <824362330@qq.com>
2025-02-15 00:39:41 +0800

    客户端改造多线程的连接和发送

--- src/EasyTcpClient/EasyTcpClient.cpp
+++ src/EasyTcpClient/EasyTcpClient.cpp
@@ -49,23 +49,27 @@ void CmdFunc()
     }  
 }
 
-int main()
+const char* server_ip = "127.0.0.1";//"192.168.26.129";//
+const unsigned short server_port = 9090;
+const int clientCount = 1000;
+const int threadCount = 4;
+EasyTcpClient* g_clients[clientCount] {};
+
+void SendThread(int id)
 {
-    //类似UI线程
-    std::thread cmdThread(CmdFunc);
-    cmdThread.detach();
+    int perClientCount = clientCount / threadCount;
+    int begin = (id-1) * perClientCount;
+    int end = id * perClientCount;
 
-    const int clientCount = 500;
-    EasyTcpClient* client[clientCount] {};
-    for(int i = 0; i < clientCount; i++)
+    for(int i = begin; i < end; i++)
     {
-        if(!g_bRun)return -1;
+        if(!g_bRun) return;
 
-        client[i] = new EasyTcpClient();
-        if(-1 == client[i]->Connect("127.0.0.1", 9090))
+        g_clients[i] = new EasyTcpClient();
+        if(-1 == g_clients[i]->Connect(server_ip, server_port))
         {
             printf("Client Connect failed!\n");
-            return -1;
+            return;
         }
     }
 
@@ -74,17 +78,45 @@ int main()
     strncpy(login.passWord, "123456", 32);
     while(g_bRun)
     {
-        for(int i = 0; i < clientCount; i++)
+        for(int i = begin; i < end; i++)
         {
-            client[i]->SendData(&login);
+            g_clients[i]->SendData(&login);
             //client[i]->OnRun();
         }
     }
 
-    for(int i = 0; i < clientCount; i++)
+    for(int i = begin; i < end; i++)
     {
-        client[i]->Close();
-        client[i] = nullptr;
+        g_clients[i]->Close();
+        g_clients[i] = nullptr;
+    }
+}
+
+int main()
+{
+    //类似UI线程
+    std::thread cmdThread(CmdFunc);
+    cmdThread.detach();
+
+    
+    std::thread workThread[threadCount];
+    for(int i = 0; i < threadCount; i++)
+    {
+        workThread[i] = std::thread(SendThread, i + 1);
+    }
+
+    for(int i = 0; i < threadCount; i++)
+    {
+        workThread[i].detach();
+    }
+
+    while(g_bRun)
+    {
+#ifdef _WIN32
+        Sleep(1000);
+#else
+        sleep(1000);
+#endif 
     }
 
     getchar();
